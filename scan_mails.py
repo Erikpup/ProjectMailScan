331814e50d36809ec9c0a9e9301a684ab07b1347
HopRison <hoprison@gmail.com>
2024-04-13 21:24:06 +0300

    v4 - ansyncio (new)

--- scan_mails.py
+++ scan_mails.py
@@ -7,12 +7,14 @@ from pdfminer.high_level import extract_text
 from docx import Document
 from io import BytesIO
 import time
+
 start_time = time.time()
+
 async def read_file(file_path):
     async with aiofiles.open(file_path, mode='rb') as f:
         content = await f.read()
     return content
-#ggggg
+
 async def process_eml(file_path):
     content = await read_file(file_path)
     msg = BytesParser(policy=policy.default).parsebytes(content)
@@ -29,28 +31,47 @@ async def process_eml(file_path):
         print(body.get_content())
 
     for part in msg.iter_parts():
-        content_type = part.get_content_type()
-        if part.get_filename() and part.get_filename().endswith('.pdf'):
-            attachment = part.get_content()
-            full_text = extract_text(BytesIO(attachment))
-            print(f"Название '.pdf' файла: {part.get_filename()}")
-            print(f"Содержимое '.pdf' файла: {full_text}")
-        if content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            # Обработка DOCX вложений
-            doc = Document(BytesIO(part.get_content()))
-            text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
-            print(f"\nТекст из DOCX вложения в файле {file_path}:")
-            print(text)
-        if content_type == "text/plain":
-            # Обработка TXT вложений
-            print(f"\nТекст из TXT вложения в файле {file_path}:")
-            print(part.get_content())
+        await check_pdf_attachment(msg, part)
+        await check_docx_attachment(msg, part)
+        await check_txt_attachment(msg, part)
+
+async def check_docx_attachment(msg, part):
+    if part.get_content_maintype() == 'multipart':
+        return
+    if part.get('Content-Disposition') is None:
+        return
+    attachment = part.get_payload(decode=True)
+    if attachment is None:
+        return
+    if part.get_filename() and part.get_filename().endswith('.docx'):
+        doc = Document(BytesIO(attachment))
+        full_text = '\n'.join([para.text for para in doc.paragraphs])
+        print("Название '.docx' файла: ", part.get_filename())
+        print("Содержимое '.docx' файла: ", full_text)
+
+async def check_txt_attachment(msg, part):
+    if part.get_filename() and part.get_filename().endswith('.txt'):
+        print(f"Название '.txt' файла: {part.get_filename()}")
+        print(f"Содержимое '.txt' файла: {part.get_content().decode('utf-16')}")
+
+async def check_pdf_attachment(msg, part):
+    if part.get_content_maintype() == 'multipart':
+        return
+    if part.get('Content-Disposition') is None:
+        return
+    attachment = part.get_payload(decode=True)
+    if attachment is None:
+        return
+    if part.get_filename() and part.get_filename().endswith('.pdf'):
+        full_text = extract_text(BytesIO(attachment))
+        print(f"Название '.pdf' файла: {part.get_filename()}")
+        print(f"Содержимое '.pdf' файла: {full_text}")
 
 async def process_directory(dir_path):
     tasks = []
     for file in os.listdir(dir_path):
         if file.endswith(".eml"):
-            task = asyncio.ensure_future(process_eml(os.path.join(dir_path, file)))
+            task = asyncio.create_task(process_eml(os.path.join(dir_path, file)))
             tasks.append(task)
     await asyncio.gather(*tasks)
 
@@ -60,4 +81,4 @@ asyncio.run(process_directory(dir_path))
 end_time = time.time()
 execution_time = end_time - start_time
 
-print(f"Время выполнения программы: {execution_time} секунд")
+print(f"Время выполнения программы: {execution_time/60} минут")
