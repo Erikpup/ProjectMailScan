89fe99bc0d9bc618b795f2ae50bf1e718251bade
LLoquer <csgopre.de@gmail.com>
2024-04-14 04:05:12 +0300

    v4.1(corrected)

--- main1.py
+++ main1.py
@@ -1,19 +1,52 @@
 import kivy
+import sqlite3
 from kivy.app import App
-from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-import sqlite3
 from kivy.uix.scrollview import ScrollView
 from kivymd.app import MDApp
 from kivymd.uix.button import MDButton, MDButtonIcon, MDButtonText
-from kivymd.uix.card import MDCard
 from kivymd.uix.textfield import MDTextField
+from kivy.uix.popup import Popup
+
+
+class AddFilterPopup(Popup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.title = "Добавить фильтр"
+        self.size_hint = (0.8, 0.6)  # Устанавливаем размер окна
+
+        # Создаем контент окна
+        content = GridLayout(cols=1)
+        self.threat_name_input = MDTextField(id="threat_name", hint_text="Имя угрозы", theme_text_color="Custom", text_color_normal="white",  text_color_focus="white")
+        self.pattern_input = MDTextField(id="pattern", hint_text="Шаблон", theme_text_color="Custom", text_color_normal="white",  text_color_focus="white")
+        add_button = MDButton(MDButtonText(text="Добавить"), on_press=self.add_filter, )
+
+
+        content.add_widget(self.threat_name_input)
+        content.add_widget(self.pattern_input)
+        content.add_widget(add_button)
+
+        self.content = content
+
+    def add_filter(self, instance):
+
+        print(self.threat_name_input.text)
+        print(self.pattern_input.text)
+
+        connection = sqlite3.connect('typeDB.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            'INSERT INTO tablepatterns (id, name, pattern) VALUES ((SELECT MAX(id) FROM tablepatterns) + 1, ?, ?)',
+            (self.threat_name_input.text, self.pattern_input.text))
+        connection.commit()
+        connection.close()
+
+        self.dismiss()
 
 
 class WindowRight(GridLayout):
@@ -29,7 +62,6 @@ class WindowRight(GridLayout):
                                         self.height / 2.))
             self.bind(pos=self.update_rect,
                       size=self.update_rect)
-
 
     def update_rect(self, *args):
         self.rect.pos = self.pos
@@ -68,7 +100,7 @@ class MainMD(GridLayout):
             self.bind(pos=self.update_rect,
                       size=self.update_rect)
 
-        left_layout = AnchorLayout(anchor_x='center', anchor_y='center', size_hint=(.5,1))
+        left_layout = AnchorLayout(anchor_x='center', anchor_y='center', size_hint=(.5, 1))
         list_layout = WindowList(cols=1, spacing=10, size_hint_y=None)
         list_layout.bind(minimum_height=list_layout.setter('height'))
         self.dict_check = {}
@@ -80,7 +112,7 @@ class MainMD(GridLayout):
             txt = Label(text=i[1], halign="left", valign="middle")
             txt.bind(size=txt.setter('text_size'))
 
-            self.dict_check.setdefault(ck,i[2])
+            self.dict_check.setdefault(ck, i[2])
 
             # Добавление объектов в список
             yacheyka.add_widget(ck)
@@ -97,9 +129,22 @@ class MainMD(GridLayout):
         right_layout = GridLayout(rows=2, spacing=20, size_hint=(.8, .9))
         right_layout_top = GridLayout(rows=2, spacing=40)
         right_layout_buttom = GridLayout(rows=2, spacing=40)
-        btn_analys = MDButton( MDButtonText( text= "Analys" , pos_hint= {"center_x": .5, "center_y": .5}),style= "tonal", pos_hint= {"center_x": .5, "center_y": .5}, theme_width= "Custom", height="56dp", size_hint_x= 1)
-        btn_add_type = MDButton(MDButtonIcon(icon= "plus"), MDButtonText( text= "Добавить фильтр" , pos_hint= {"center_x": .5, "center_y": .5}),style= "tonal", pos_hint= {"center_x": .5, "center_y": .5}, theme_width= "Custom", height="56dp", size_hint_x= 1)
-        btn_remove_type = MDButton(MDButtonIcon(icon= "trash-can"), MDButtonText( text= "Удалить фильтр" , pos_hint= {"center_x": .5, "center_y": .5}),style= "tonal", pos_hint= {"center_x": .5, "center_y": .5}, theme_width= "Custom", height="56dp", size_hint_x= 1)
+        btn_analys = MDButton(MDButtonText(text="Analys", pos_hint={"center_x": .5, "center_y": .5}), style="tonal",
+                              pos_hint={"center_x": .5, "center_y": .5}, theme_width="Custom", height="56dp",
+                              size_hint_x=1)
+        btn_add_type = MDButton(
+            MDButtonIcon(icon="plus"), MDButtonText(text="Добавить фильтр", pos_hint={"center_x": .5, "center_y": .5}),
+            style="tonal",
+            pos_hint={"center_x": .5, "center_y": .5},
+            theme_width="Custom",
+            height="56dp",
+            size_hint_x=1,
+        )
+        btn_add_type.bind(on_press=self.add_filter)
+        btn_remove_type = MDButton(MDButtonIcon(icon="trash-can"),
+                                   MDButtonText(text="Удалить фильтр", pos_hint={"center_x": .5, "center_y": .5}),
+                                   style="tonal", pos_hint={"center_x": .5, "center_y": .5}, theme_width="Custom",
+                                   height="56dp", size_hint_x=1)
         input_path = MDTextField(text="Путь к данным")
 
         right_layout_top.add_widget(btn_add_type)
@@ -118,6 +163,9 @@ class MainMD(GridLayout):
         self.rect.pos = self.pos
         self.rect.size = self.size
 
+    def add_filter(self, instance):
+        popup = AddFilterPopup()
+        popup.open()
 
     def open_data_db(self):
         # Устанавливаем соединение с базой данных
@@ -132,25 +180,14 @@ class MainMD(GridLayout):
         # Выводим результаты
         return types
 
-
     def analys(self):
         pass
 
-# Defining a class
+
 class MyFirstKivyApp(MDApp):
 
-    # Function that returns
-    # the root widget
     def build(self):
-        # Label with text Hello World is
-        # returned as root widget
-
         return MainMD()
 
-    # Here our class is initialized
 
-
-# and its run() method is called.
-# This initializes and starts
-# our Kivy application.
 MyFirstKivyApp().run()
